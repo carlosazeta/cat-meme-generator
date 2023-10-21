c420268ca6bcc7f1b932a223617019b08afff168
carlosazeta <carlosagundezdev@gmail.com>
2023-10-21 06:44:21 +0200

    Create the app

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import './App.css'
+
+function App () {
+  const [inputValue, setInputValue] = useState('')
+  const [imgUrl, setImgUrl] = useState('')
+
+  const handleChange = (event) => {
+    const newInputText = event.target.value
+
+    if (newInputText.startsWith(' ')) {
+      setInputValue(newInputText.trim())
+    } else {
+      setInputValue(newInputText)
+    }
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    if (!inputValue) return
+
+    fetch(`https://cataas.com/cat/says/${inputValue}?fontSize=50&fontColor=white`)
+      .then((response) => response
+      )
+      .then((data) => {
+        const { url } = data
+        setImgUrl(url)
+      })
+  }
+
+  return (
+    <>
+      <h1>RANDOM CAT GENERATOR</h1>
+      <form onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <input
+            className='input input-alt'
+            type='text' onChange={handleChange}
+            value={inputValue}
+            name='input'
+            placeholder='Write here...'
+          />
+          <span className='input-border input-border-alt' />
+          <button type='submit'>Generate cat meme</button>
+        </div>
+      </form>
+      <main>
+        {imgUrl && <img src={imgUrl} alt='A random meme cat' />}
+      </main>
+    </>
+  )
+}
+
+export default App
